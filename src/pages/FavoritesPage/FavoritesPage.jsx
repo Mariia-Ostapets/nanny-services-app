b3895167d4490e0290a4d99d8f959e3b220f1ced
Mariia_Ostapets <mariyaostapets@gmail.com>
2025-04-02 19:40:25 +0300

    Add structure for FavoritesPage, link PrivateRoute

--- src/pages/FavoritesPage/FavoritesPage.jsx
+++ src/pages/FavoritesPage/FavoritesPage.jsx
@@ -1,5 +1,16 @@
 import css from './FavoritesPage.module.css';
+import Filters from '../../components/Filters/Filters';
+import NanniesList from '../../components/NanniesList/NanniesList';
+import Button from '../../components/ui/Button/Button';
 
 export default function FavoritesPage() {
-  return;
+  return (
+    <div className={css.favoritesPageContainer}>
+      <Filters />
+      <NanniesList />
+      <Button type="button" variant="loadMore">
+        Load more
+      </Button>
+    </div>
+  );
 }
